c7d2e47a9bac8de26163da4e006fd2601b71cfa0
Nick <nsagias@gmail.com>
2022-01-06 14:28:38 -0500

    Add connectionString for sqlite to project

--- TodoApiDB/Program.cs
+++ TodoApiDB/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todos.db";
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("items"));
+// builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<TodoDb>(connectionString);
 builder.Services.AddSwaggerGen(c => {
   c.SwaggerDoc("v1", new OpenApiInfo {
     Title = "Todo API EntityFramework Core",
