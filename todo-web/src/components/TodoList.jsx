43a680bf23dac4eb302db4d9fb11e430a3316eed
Nick <nsagias@gmail.com>
2022-01-06 17:18:03 -0500

    Add code to todolist

--- todo-web/src/components/TodoList.jsx
+++ todo-web/src/components/TodoList.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 
 export default function TodoList() {
-
+  const parsedTodos = Array.isArray(todos) 
+    && todos.map(todo => <TodoItem todo={todo} />);
   return (
     <>
+      {parsedTodos}
     </>
   );
 }
