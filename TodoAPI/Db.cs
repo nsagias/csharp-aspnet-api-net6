5216fcdf9416e50191ab42a1535b9a0ba642d081
Nick <nsagias@gmail.com>
2022-01-05 21:07:10 -0500

    Add create mock data todoList

--- TodoAPI/Db.cs
+++ TodoAPI/Db.cs
@@ -6,3 +6,11 @@ public record Todo {
   public string? Name { get; set;}
 }
 
+public class TodoDB {
+  // create mock dataList
+  private static List<Todo> _todosList = new List<Todo>() {
+    new Todo{ Id=1, Name="Learn minimal c# api" },
+    new Todo{ Id=2, Name="Learn how the use it K8s" },
+    new Todo{ Id=3, Name="Learn how to lauch on Azure" }
+  };
+}
