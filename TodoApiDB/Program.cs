bea3866f1c2b01810ecc767446f852e54fc12ab0
Nick <nsagias@gmail.com>
2022-01-06 00:19:53 -0500

    Add create/post verb async

--- TodoApiDB/Program.cs
+++ TodoApiDB/Program.cs
@@ -19,7 +19,15 @@ app.UseSwaggerUI(c => {
   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API EntityFramework V1");
 });
 
+// home
 app.MapGet("/", () => "Hello World!");
+// get all todos
 app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+// create new tody
+app.MapPost("/todos", async (TodoDb db, Todo todo) => {
+  await db.Todos.AddAsync(todo);
+  await db.SaveChangesAsync();
+  return Results.Created($"/todos/{todo.Id}", todo);
+});
 
 app.Run();
