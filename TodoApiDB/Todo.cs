1e8c19234edf3d4ac6bc519db6102973b63727fc
Nick <nsagias@gmail.com>
2022-01-06 00:03:23 -0500

    add dbcontext to todo.cs class

--- TodoApiDB/Todo.cs
+++ TodoApiDB/Todo.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace TodoApiDB.Models {
   public class Todo {
     public int Id { get; set; }
     public string? Name { get; set;}
     public string? Description {get; set;}
+  }
+
+  class TodoDB : DbContext {
+    public TodoDB(DbContextOptions options) : base(options) {}
+    public DbSet<Todo> Todos {get; set;}
   }
 }
