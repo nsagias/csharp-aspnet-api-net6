830d8cd6f72581418b143a824ce367308565a1ba
Nick <nsagias@gmail.com>
2022-01-05 19:49:11 -0500

    update file and location of swagger

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+  {
+      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+  });
 builder.Services.AddCors(options => {});
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment()) {
+  app.UseDeveloperExceptionPage();
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+  {
+     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nick API V1");
+  });
 
-app.MapGet("/", () => "Hello World!");
 app.UseCors("some unique string");
+app.MapGet("/", () => "Hello World!");
+
 app.Run();
