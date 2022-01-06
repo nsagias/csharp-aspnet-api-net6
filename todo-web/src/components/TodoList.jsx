c578aa690042fd9574b790fafa68ca8445239c9f
Nick <nsagias@gmail.com>
2022-01-06 17:39:53 -0500

    Add props to TodoList, import TodoList to App

--- todo-web/src/components/TodoList.jsx
+++ todo-web/src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 
-export default function TodoList() {
+export default function TodoList(props) {
   const parsedTodos = Array.isArray(todos) 
     && todos.map(todo => <TodoItem todo={todo} />);
   return (
