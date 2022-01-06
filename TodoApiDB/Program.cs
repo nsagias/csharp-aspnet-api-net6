c2d3871ef74f8aa1187b7663e7449e9e485541eb
Nick <nsagias@gmail.com>
2022-01-05 23:54:26 -0500

    Add Swagger descriptions and config

--- TodoApiDB/Program.cs
+++ TodoApiDB/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+  c.SwaggerDoc("v1", new OpenApiInfo {
+    Title = "Todo API EntityFramework Core",
+    Description = "API with Database",
+    Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API EntityFramework V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
