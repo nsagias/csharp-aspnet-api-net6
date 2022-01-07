3948dad728b9a404869d700d70c94312ef11513f
Nick <nsagias@gmail.com>
2022-01-06 19:52:03 -0500

    Separate out db connection from model

--- TodoApiDB/Todo.cs
+++ TodoApiDB/Todo.cs
@@ -1,14 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace TodoApiDB.Models {
   public class Todo {
     public int Id { get; set; }
     public string? Name { get; set;}
     public string? Description {get; set;}
-  }
-
-  class TodoDb : DbContext {
-    public TodoDb(DbContextOptions options) : base(options) {}
-    public DbSet<Todo> Todos {get; set;}
   }
 }
--- TodoApiDB/TodoDb.cs
+++ TodoApiDB/TodoDb.cs
@@ -6,4 +6,8 @@ namespace TodoApiDB.Data;
 class TodoDb : DbContext {
   public TodoDb(DbContextOptions options) : base(options) {}
   public DbSet<Todo> Todos {get; set;}
+
+  protected override void onModelCreating (ModelBuilder modelBuilder) {
+    
+  }
 }
