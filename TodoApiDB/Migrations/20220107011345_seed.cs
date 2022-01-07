209621a3c49a1937c4b9470d399c415cfd4a2d74
Nick <nsagias@gmail.com>
2022-01-06 20:14:28 -0500

    Add seed using dotnet ef migrations add seed

---
+++ TodoApiDB/Migrations/20220107011345_seed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApiDB.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "The fun never stops!", "Refactor TodoAPI" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
